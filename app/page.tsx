c5d2a517c17e426b722fe430eecb9870a52b6667
enesbkoca <enesbkoca@gmail.com>
2024-12-08 19:08:48 +0100

    Fix apostrophe and rollback to display jobdescription

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[70vh] p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <h1 className="text-2xl font-bold">Let's ace your interview</h1>
+          <h1 className="text-2xl font-bold">Let&apos;s ace your interview</h1>
           <p className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)] mb-4">
             Enter the job description, and we will generate interview questions to help you prepare.
           </p>
@@ -40,9 +40,9 @@ export default function Home() {
               onGenerateQuestions={generateQuestions}
           />
 
-          {/*<JobDescriptionDisplay*/}
-          {/*    submittedJobDescription={submittedJobDescription}*/}
-          {/*/>*/}
+          <JobDescriptionDisplay
+              submittedJobDescription={submittedJobDescription}
+          />
 
           <InterviewQuestionsList
               questions={interviewQuestions}
