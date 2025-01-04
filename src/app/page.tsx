e72bba0a449ba486b9f16541b8e226cf42402e3f
Berkay_HP <enesbkoca@gmail.com>
2025-01-04 15:02:56 +0100

    Fix character error by escaping apostraphre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] relative">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <h1 className="text-2xl font-bold">Let's ace your interview</h1>
+                <h1 className="text-2xl font-bold">Let&apos;s ace your interview</h1>
 
                 <p className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)] mb-4">
                     Enter the job description, and we will generate interview questions to help you prepare.
