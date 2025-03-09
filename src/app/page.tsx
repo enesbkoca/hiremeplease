dde7dc65eb31336586af056f81af636dab28d166
Berkay_HP <enesbkoca@gmail.com>
2025-03-09 14:18:02 +0100

    Remove unnecessary import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { UserInput } from '@/components/UserInput';
 import { useLoading } from '@/context/LoadingContext';
