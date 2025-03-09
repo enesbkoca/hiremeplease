f8d38efd03c20a541c48f9d4778ee26fb7a86e92
Mustafa Talha <mtilerisoy@gmail.com>
2025-03-09 12:33:13 +0100

    implement Supabase OTP login

---
+++ src/app/(auth)/profile/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { Session } from '@supabase/supabase-js'
+import { useRouter } from 'next/navigation'
+import { supabase } from '@/utils/supabase'
+
+export default function Profile() {
+    const router = useRouter();
+    const [session, setSession] = useState<Session | null>(null)
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session)
+        })
+    }, []);
+
+    if (!session) {
+        return <div>Loading...</div> //router.push('/signin')
+    }
+
+    return (
+        <div className="flex flex-col items-center gap-4">
+            <div className="text-white">Logged in as {session.user.email}</div>
+            <button
+                onClick={() => {
+                    supabase.auth.signOut()
+                    router.push('/signup')
+                }}
+                className="px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+                Sign Out
+            </button>
+        </div>
+    )
+}
---
+++ src/app/(auth)/signup/otp-verification/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { useRouter } from 'next/navigation'
+import OtpVerification from './OtpVerification'
+
+// Create a supabase client
+const supaURL : string = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supaAnonKey : string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+const supabase = createClient(supaURL, supaAnonKey)
+
+export default function OtpPage() {
+  const [error, setError] = useState<string | null>(null)
+  const router = useRouter();
+  const [email, setEmail] = useState<string>('');
+
+  useEffect(() => {
+    const emailParam = new URLSearchParams(window.location.search).get('email');
+    if (emailParam) {
+      setEmail(decodeURIComponent(emailParam));
+    }
+  }, []);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-md w-full space-y-8 p-8 rounded-xl shadow-lg">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
+            Verify your OTP
+          </h2>
+        </div>
+        {error && (
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+            {error}
+          </div>
+        )}
+        <OtpVerification email={email} onBack={() => router.push('/signup')} supabase={supabase} />
+      </div>
+    </div>
+  )
+}
