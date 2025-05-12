87409f07af289994bed3ac1d84db00f8132764c1
Berkay_HP <enesbkoca@gmail.com>
2025-05-12 13:02:51 +0200

    Remove caching of redis conn

--- api/utils/redis_conn.py
+++ api/utils/redis_conn.py
@@ -9,21 +9,17 @@ from api.utils.logger_config import get_logger
 logger = get_logger()
 load_dotenv()
 
-_redis_conn = None
 
 def get_redis_conn():
     """
-    Returns the Redis connection object. Initializes the connection on the first call.
-    Subsequent calls return the existing connection.
+    Returns the Redis connection object.
     """
-    global _redis_conn
 
-    if _redis_conn is None:
-        try:
-            redis_conn = Redis.from_url(os.getenv("REDIS_URL"))
-            q = Queue("gpt_response", connection=redis_conn)
-            logger.info("Successfully connected to Redis and initialized queue")
-        except Exception as e:
-            logger.error(f"Failed to connect to Redis: {str(e)}")
-            raise
-    return _redis_conn
+    try:
+        redis_conn = Redis.from_url(os.getenv("REDIS_URL"))
+        q = Queue("gpt_response", connection=redis_conn)
+        logger.info("Successfully connected to Redis and initialized queue")
+    except Exception as e:
+        logger.error(f"Failed to connect to Redis: {str(e)}")
+        raise
+    return redis_conn
