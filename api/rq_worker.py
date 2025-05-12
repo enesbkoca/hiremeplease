b654b2cd80ca402415711bc525e1e5aece49ce0b
Berkay_HP <enesbkoca@gmail.com>
2025-05-12 13:20:35 +0200

    Add necessary import

--- api/rq_worker.py
+++ api/rq_worker.py
@@ -5,6 +5,7 @@ from rq import Worker
 
 from api.utils.redis_conn import get_redis_conn
 from api.utils.logger_config import get_logger
+from api.llm_calls import generate_response
 
 logger = get_logger()
 redis_conn = get_redis_conn()
