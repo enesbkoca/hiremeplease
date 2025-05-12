9f4dd4f8798ab062092f9a63901a267a19946e0a
Berkay_HP <enesbkoca@gmail.com>
2025-05-12 13:45:29 +0200

    fix

--- api/index.py
+++ api/index.py
@@ -6,12 +6,37 @@ import requests
 from rq import Queue
 from flask import Flask, request, jsonify
 
-from api.utils.services import generate_and_store_questions
 from api.utils.redis_conn import get_redis_conn
-from api.llm_calls import generate_answer_analysis
+from api.llm_calls import generate_answer_analysis, generate_response
 from api.utils.logger_config import get_logger
 
 logger = get_logger()
+redis_conn = get_redis_conn()
+
+def generate_and_store_questions(description_id, description):
+    try:
+        # Load job data and set status to processing
+        logger.debug(f"Processing job {description_id}")
+        job_data = json.loads(redis_conn.hget("jobs", description_id))
+        job_data["status"] = "Processing"
+        redis_conn.hset("jobs", description_id, json.dumps(job_data))
+
+        # Generate response
+        logger.info(f"Generating response for job {description_id}")
+        response = generate_response(description)
+
+        # When response are generated set status to completed and save them
+        job_data["status"] = "Completed"
+        job_data["results"] = response
+
+        redis_conn.hset("jobs", description_id, json.dumps(job_data))
+        logger.info(f"Successfully completed job {description_id}")
+    except Exception as e:
+        logger.error(f"Error processing job {description_id}: {str(e)}")
+        job_data["status"] = "Failed"
+        job_data["error"] = str(e)
+        redis_conn.hset("jobs", description_id, json.dumps(job_data))
+        raise
 
 app = Flask(__name__)
 
--- api/rq_worker.py
+++ api/rq_worker.py
@@ -2,20 +2,22 @@ import os
 
 from redis import Redis
 from rq import Worker
+from dotenv import load_dotenv
 
-from api.llm_calls import generate_response
-from api.utils.redis_conn import get_redis_conn
-from api.utils.logger_config import get_logger
-from api.utils.services import generate_and_store_questions
-
-
+from .llm_calls import generate_response
+from .utils.logger_config import get_logger
 
 logger = get_logger()
-redis_conn = get_redis_conn()
+
+load_dotenv()
 
 try:
+    redis_conn = Redis.from_url(os.getenv("REDIS_URL"))
+    logger.info("Successfully connected to Redis in worker")
+
     w = Worker(['gpt_response'], connection=redis_conn)
     logger.info("Worker initialized and ready to process jobs")
+
     w.work()
 except Exception as e:
     logger.error(f"Failed to initialize worker: {str(e)}")
