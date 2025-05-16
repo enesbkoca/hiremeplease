19489c3caa3e4739b56330c66723fd81b67176f7
enesbkoca <enesbkoca@gmail.com>
2025-05-16 13:48:39 +0200

    Replace api request with apiClient axios wrapper

--- src/api.ts
+++ src/api.ts
@@ -11,25 +11,31 @@ apiClient.interceptors.request.use(
             const { data: { session }, error: sessionError } = await supabase.auth.getSession();
 
             if (sessionError) {
-                console.error("Error getting Supabase session for API Request: ", sessionError.message);
-
-                return Promise.reject(sessionError);
-            }
-
-            if (session?.access_token) {
+                console.warn(
+                    "Supabase: Error fetching session, request will proceed without auth header:",
+                    sessionError.message
+                );
+            } else if (session?.access_token) {
                 config.headers.Authorization = `Bearer ${session.access_token}`;
+                config.headers["x-refresh-token"] = session.refresh_token;
+
             } else {
-                console.error("No access token found in Supabase session.");
+                console.warn(
+                    "Supabase: No active session or access token found, request will proceed without auth header."
+                );
             }
 
         } catch (e) {
-            console.error("Error getting Supabase session for Axios interceptor: ", e);
-            return Promise.reject(e);
+            console.error(
+                "Axios Interceptor: Unexpected error trying to get Supabase session, request will proceed without auth header:",
+                e
+            );
         }
 
         return config;
     },
     (error) => {
+        console.error("Axios request interceptor error (before session check):", error);
         return Promise.reject(error);
     }
 );
